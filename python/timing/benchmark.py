3d41d8bbb1430a17b08076b92ba3b41ec066527c
Jokeren <robinho364@gmail.com>
2022-05-04 12:19:13 -0500

    Finish profile script

--- python/timing/benchmark.py
+++ python/timing/benchmark.py
@@ -2,41 +2,33 @@
 
 import torch
 import torch.nn as nn
-from typing import List
-from SimpleModel import SimpleModel
-from converter import cast_bytes_to_memory_string
-from decorators import timing
-from autotuning import autotune
-
-from tqdm import tqdm as tq
 import pandas as pd
 import numpy as np
-import pdb
+import argparse
+
+from typing import List
+from simple_model import SimpleModel
+from autotuning import autotune, load_configs
+from utils import *
+
+from tqdm import tqdm as tq
 from tabulate import tabulate
 
 from rz_linear.RzLinearFunction import controls
 
+
+DEFAULT_MEM_SIZES = [1024*1024, 1024*1024*8, 1024 *
+                     1024*16, 1024*1024*32, 1024*1024*64, 1024*1024*128]
 MAX_ITERS = 10
 WARMUP_ITERS = 2
 
 
-def count_parameters(model):
-    num = np.sum([p.numel() for p in model.parameters()])
-    return num
-
-
-def benchmark(shapes: List[List[int]], batch_sizes: List[int], mem_size: List[int]) -> pd.DataFrame:
+def benchmark(shapes: List[List[int]], batch_sizes: List[int], mem_sizes: List[int], optimizers: List[str]) -> pd.DataFrame:
     """ Benchmark compressed embedding table speed """
-
     report = pd.DataFrame()
     model_names = ["Full", "HNet", "ROBE-Sketch"]
     hnet = [None, True, False]
-
-    optimizers = [
-        'sgd',
-        'adagrad',
-        'adam'
-    ]
+    loss_fct = nn.MSELoss().to(device=get_device())
 
     for optimizer_name in optimizers:
         for bs in tq(batch_sizes):
@@ -45,14 +37,14 @@ def benchmark(shapes: List[List[int]], batch_sizes: List[int], mem_size: List[in
                     model_name = model_names[i]
                     is_hnet = hnet[i]
                     is_robez = (model_name != "Full")
-                    for j, mem in enumerate(mem_size):
+                    for j, mem in enumerate(mem_sizes):
                         if model_name == "Full":
                             mem = shape[0] * shape[1]
                             if j > 0:
                                 continue
 
                         model = SimpleModel(
-                            shape[0], shape[1], mem, is_robez, is_hnet).cuda(0)
+                            shape[0], shape[1], mem, is_robez, is_hnet).to(get_device())
                         if optimizer_name == "sgd":
                             optimizer = torch.optim.SGD(
                                 model.parameters(), lr=0.001)
@@ -65,31 +57,28 @@ def benchmark(shapes: List[List[int]], batch_sizes: List[int], mem_size: List[in
                         else:
                             raise NotImplementedError
 
-                        loss_fct = nn.MSELoss().cuda(0)
-
                         model.train()
 
                         def train(iters, x, y, forward_times=None, backward_times=None, opt_times=None):
-                            # forward
-                            t, y_pred = timing(model)(x)
-                            if forward_times is not None:
-                                forward_times.append(t)
-                            # loss
-                            loss_value = loss_fct(y, y_pred)
-                            # grad
-                            t, _ = timing(loss_value.backward)()
-                            if backward_times is not None:
-                                backward_times.append(t)
-                            # opt
-                            t, _ = timing(optimizer.step)()
-                            if opt_times is not None:
-                                opt_times.append(t)
-                            optimizer.zero_grad()
+                            for _ in range(iters):
+                                # forward
+                                t, y_pred = timing(model)(x)
+                                if forward_times is not None:
+                                    forward_times.append(t)
+                                # loss
+                                loss_value = loss_fct(y, y_pred)
+                                # grad
+                                t, _ = timing(loss_value.backward)()
+                                if backward_times is not None:
+                                    backward_times.append(t)
+                                # opt
+                                t, _ = timing(optimizer.step)()
+                                if opt_times is not None:
+                                    opt_times.append(t)
+                                optimizer.zero_grad()
 
-                        x = torch.from_numpy(np.random.rand(
-                            bs, shape[0])).float().cuda(0)
-                        y = torch.from_numpy(np.random.uniform(
-                            size=(bs, 1))).float().cuda(0)
+                        x = torch.rand((bs, shape[0]), device=get_device())
+                        y = torch.rand((bs, 1), device=get_device())
 
                         forward_pass = []
                         backward_pass = []
@@ -115,37 +104,75 @@ def benchmark(shapes: List[List[int]], batch_sizes: List[int], mem_size: List[in
                                 "backward(ms)": [backward_pass],
                                 "optcomp(ms)": [opt_computation],
                                 "total(ms)": [forward_pass + backward_pass + opt_computation],
-                                "msize": [cast_bytes_to_memory_string(4 * count_parameters(model))],
+                                "msize": [get_model_bytes(model)],
                                 "optim": [optimizer_name],
                             }
                         )
-                        print(infos)
+                        vprint(infos)
                         report = pd.concat([report, infos])
-                        report.to_csv("temp.csv", index=False)
 
     return report
 
-    # pylint: disable=redefined-outer-name
-if __name__ == "__main__":
-    torch.backends.cuda.matmul.allow_tf32 = False
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-a', '--autotune', action='store_true', default=False)
+parser.add_argument('-tf32', '--tf32', action='store_true', default=False)
+parser.add_argument('-op', '--optimizer',
+                    choices=['all', 'sgd', 'adagrad', 'adam'], default='all')
+parser.add_argument('-s', '--save', type=str,
+                    help='Save autotuning configurations', required=False, default='')
+parser.add_argument('-l', '--load', type=str,
+                    help='Load autotuning configurations', required=False, default='')
+parser.add_argument('-o', '--output', type=str,
+                    help='Output benchmark file', default='bechmark.csv')
+parser.add_argument('-d', '--dims', type=str, default='10240',
+                    help='matrix feature dims, separated by ,')
+parser.add_argument('-b', '--batch-sizes', type=str,
+                    default='512', help='batch sizes, separated by')
+parser.add_argument('-c', '--cuda', type=str, default='cuda:0')
+parser.add_argument('-v', '--verbose', action='store_true')
+# TODO(Keren): make mem_sizes configurable
+args = parser.parse_args()
+
+
+def str_to_int_list(s, d=','):
+    l = s.split(d)
+    res = map(int, l)
+    return list(res)
+
+
+def dims_to_shapes(dims):
+    return [[dim, dim] for dim in dims]
+
+
+set_verbose(args.verbose)
+set_device(args.cuda)
+
+controls['triton_allow_autotune'] = False
+
+shapes = dims_to_shapes(str_to_int_list(args.dims))
+batch_sizes = str_to_int_list(args.batch_sizes)
+
+if args.tf32:
+    controls['triton_allow_tf32'] = True
+    torch.backends.cuda.matmul.allow_tf32 = True
+else:
     controls['triton_allow_tf32'] = False
-    controls['triton_allow_autotune'] = False
-    # benchmark(shapes : List[List[int]], batchsizes: List[int], mem_size: List[int]) -> pd.DataFrame:
-    # shapes = [[1024, 1024], [10240,10240]]
-    shapes = [[10240, 10240]]
-    # batch_sizes = [64, 512, 4096, 32768]
-    # batch_sizes = [64, 512, 1024, 4096, 10240]
-    # batch_sizes = [64, 512, 4096]
-    batch_sizes = [512]
+    torch.backends.cuda.matmul.allow_tf32 = False
 
-    # mem_size = [1, 8, 64, 512] + [32768 * 8 ** i for i in range(5)] + [nb_embeddings*embeding_dim]
-    # mem_size = [64, 512] + [32768 * 8 ** i for i in range(5)]
-    mem_sizes = [1024*1024, 1024*1024*8, 1024*1024 *
-                 16, 1024*1024*32, 1024*1024*64, 1024*1024*128]
-    # mem_size=[1024*1024, 1024*1024*8]
+if args.load != '':
+    load_configs(file_name=args.load)
+elif args.autotune:
+    autotune(batch_sizes=batch_sizes, shapes=shapes, mem_sizes=DEFAULT_MEM_SIZES,
+             file_name=args.save, allow_tf32=controls['triton_allow_tf32'], )
 
-    # autotune(batch_sizes=batch_sizes, shapes=shapes,
-    #         mem_sizes=mem_sizes, allow_tf32=controls['triton_allow_tf32'])
-    my_report = benchmark(shapes, batch_sizes, mem_sizes)
-    my_report.to_csv("benchmark.csv", index=False)
-    print(tabulate(my_report, headers='keys', tablefmt='github'))
+if args.optimizer == 'all':
+    optimizers = ['sgd', 'adagrad', 'adam']
+else:
+    optimizers = [args.optimizer]
+
+report = benchmark(shapes=shapes, batch_sizes=batch_sizes,
+                   mem_sizes=DEFAULT_MEM_SIZES, optimizers=optimizers)
+report.to_csv(args.output, index=False)
+
+vprint(tabulate(report, headers='keys', tablefmt='github'))
